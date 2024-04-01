b28b632f7c596922b077debfdd06c74f27ea4344
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-04-01 10:07:55 -0400

    comments and refactoring

--- example_cavity.py
+++ example_cavity.py
@@ -2,9 +2,43 @@ import festim as F
 from ufl import exp
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class CavityReaction(F.Reaction):
+    """A reaction between mobile particles and cavity traps
+
+    Args:
+        N_t_eff (F.Species): effective trap concentration
+        mobile (F.Species): mobile species
+        product (F.Species): trapped species
+        R (float): cavity radius (m)
+        lambda_ (float): mean free path (m)
+        a_m (float): surface density of mobile particles
+        nu_bs (float): attempt frequency for trapping
+        E_BS (float): activation energy for the bulk-to-cavity surface transition (eV)
+        nu_sb (float): attempt frequency for detrapping
+        E_SB (float): activation energy for the cavity surface-to-bulk transition (eV)
+        D_0 (float): diffusion coefficient pre-exponential factor (m2/s)
+        E_D (float): diffusion activation energy (eV)
+        volume (F.VolumeSubdomain1D): the volume subdomain where the reaction takes place
+
+    Attributes:
+        N_t_eff (F.Species): effective trap concentration
+        mobile (F.Species): mobile species
+        R (float): cavity radius (m)
+        lambda_ (float): mean free path (m)
+        a_m (float): surface density of mobile particles
+        nu_bs (float): attempt frequency for trapping
+        E_BS (float): activation energy for the bulk-to-cavity surface transition (eV)
+        nu_sb (float): attempt frequency for detrapping
+        E_SB (float): activation energy for the cavity surface-to-bulk transition (eV)
+        D_0 (float): diffusion coefficient pre-exponential factor (m2/s)
+        E_D (float): diffusion activation energy (eV)
+        volume (F.VolumeSubdomain1D): the volume subdomain where the reaction takes place
+
+    """
+
     def __init__(
         self,
         N_t_eff: F.Species,
@@ -37,12 +71,12 @@ class CavityReaction(F.Reaction):
 
         super().__init__(
             reactant1=mobile,
-            reactant2=F.ImplicitSpecies(n=0),
+            reactant2=F.ImplicitSpecies(n=0),  # ignored
             product=product,
-            k_0=0,
-            E_k=0,
-            p_0=0,
-            E_p=0,
+            k_0=0,  # ignored
+            E_k=0,  # ignored
+            p_0=0,  # ignored
+            E_p=0,  # ignored
             volume=volume,
         )
 
@@ -54,24 +88,35 @@ class CavityReaction(F.Reaction):
         """Eq. 20
 
         Args:
-            c_t (_type_): _description_
-            temperature (_type_): _description_
+            c_t (dolfinx.Function): the trapped concentration
+            temperature (dolfinx.Function or float): the temperature in K
 
         Returns:
-            _type_: _description_
+            ufl.expression: the omega term
         """
         return 1 + self.nu_bs * self.lambda_ * self.R / self.D_0 * (
             1 - c_t / self.N_t_eff
         ) * exp((self.E_D - self.E_BS) / (F.k_B * temperature))
 
     def reaction_term(self, temperature):
+        """
+        Reaction term between mobile particles and cavity traps
+        Reduced model in the case of no gradient in H potential energy landscape
+        Eq. 23
+
+        Args:
+            temperature (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         c_t = self.product.concentration
         mobile = self.mobile.concentration
 
         omega = self.omega(c_t=c_t, temperature=temperature)
 
         if self.R == 0:
-            trapping_rate = 0
+            trapping_rate = 0  # TODO check if this is really the case
         else:
             trapping_rate = (
                 4
@@ -116,35 +161,11 @@ my_model.subdomains = [
     right_surface,
 ]
 
-# -------- Hydrogen species and reactions --------- #
+# -------- Hydrogen species --------- #
 
 mobile_H = F.Species("H")
-trapped_H = F.Species("trapped_H1", mobile=False)
-
-N_t_eff = 1e-10 * w_atom_density  # m-3
-lambda_val = 1.12e-10  # m
-
+trapped_H = F.Species("trapped_H", mobile=False)
 my_model.species = [mobile_H, trapped_H]
-
-my_model.reactions = [
-    CavityReaction(
-        N_t_eff=N_t_eff,
-        mobile=mobile_H,
-        product=trapped_H,
-        R=1e-8,  # m,
-        lambda_=lambda_val,
-        a_m=6 * w_atom_density * lambda_val,  # at / W * W/m3 * m = at/m2,
-        nu_bs=tungsten.D_0 / lambda_val**2,
-        E_BS=tungsten.E_D,
-        nu_sb=1e13,
-        E_SB=1.5,
-        E_D=tungsten.E_D,
-        D_0=tungsten.D_0,
-        volume=my_subdomain,
-    )
-]
-
-my_model.initial_conditions = [F.InitialCondition(value=N_t_eff, species=trapped_H)]
 
 # -------- Temperature --------- #
 
@@ -171,12 +192,7 @@ my_model.boundary_conditions = [
 left_flux = F.SurfaceFlux(field=mobile_H, surface=left_surface)
 right_flux = F.SurfaceFlux(field=mobile_H, surface=right_surface)
 
-my_model.exports = [
-    F.XDMFExport("cavity/mobile_concentration.xdmf", field=mobile_H),
-    F.XDMFExport("cavity/trapped_concentration.xdmf", field=trapped_H),
-    left_flux,
-    right_flux,
-]
+my_model.exports = [left_flux, right_flux]
 
 # -------- Settings --------- #
 
@@ -187,28 +203,63 @@ my_model.settings = F.Settings(
     final_time=(final_temp - implantation_temp) / temperature_ramp,
 )
 
-my_model.settings.stepsize = F.Stepsize(initial_value=2)
+my_model.settings.stepsize = F.Stepsize(initial_value=10)
 
 # -------- Run --------- #
 
-my_model.initialise()
+fig, axs = plt.subplots(2, 1, sharex=True)
 
-my_model.run()
+for ax, N_t_eff in zip(axs, [1e-10 * w_atom_density, 1e-2 * w_atom_density]):
+    plt.sca(ax)
+    plt.title("$N_{t, eff}$ =" + f"{N_t_eff/w_atom_density:.0e} at.fr")
+    plt.ylabel("Outgassing flux (m-2s-1)")
 
-# -------- Save results --------- #
+    for R in [0, 1e-9, 1e-8]:
+        lambda_val = 1.12e-10  # m
 
-np.savetxt(
-    "outgassing_flux_tds.txt",
-    np.array(left_flux.data) + np.array(right_flux.data),
-)
-np.savetxt("times_tds.txt", np.array(left_flux.t))
+        my_model.reactions = [
+            CavityReaction(
+                N_t_eff=N_t_eff,
+                mobile=mobile_H,
+                product=trapped_H,
+                R=R,  # m
+                lambda_=lambda_val,
+                a_m=6 * w_atom_density * lambda_val,  # at / W * W/m3 * m = at/m2
+                nu_bs=tungsten.D_0 / lambda_val**2,
+                E_BS=tungsten.E_D,
+                nu_sb=1e13,
+                E_SB=1.5,
+                E_D=tungsten.E_D,
+                D_0=tungsten.D_0,
+                volume=my_subdomain,
+            )
+        ]
 
-import matplotlib.pyplot as plt
+        # fill the traps with hydrogen
+        my_model.initial_conditions = [
+            F.InitialCondition(value=N_t_eff, species=trapped_H)
+        ]
 
-plt.plot(
-    temp_function(np.array(left_flux.t)),
-    np.array(left_flux.data) + np.array(right_flux.data),
-)
+        # data of derived quantities needs to be manually reset issue #728
+        left_flux.data = []
+        left_flux.t = []
+        right_flux.data = []
+        right_flux.t = []
+
+        # initialise and run model
+        my_model.initialise()
+        my_model.run()
+
+        # plot the results
+        plt.plot(
+            temp_function(np.array(left_flux.t)),
+            np.array(left_flux.data) + np.array(right_flux.data),
+            label=f"R = {R*1e9:.0f} nm",
+        )
+    plt.legend()
+    plt.ylim(bottom=0)
+
+
 plt.xlabel("T (K)")
-plt.ylabel("Outgassing flux (m-2s-1)")
+plt.savefig("cavity/tds.png")
 plt.show()
